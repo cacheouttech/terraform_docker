0099137a70b77f595a3919281c86ff45ba367849
cacheouttech <108108554+cacheouttech@users.noreply.github.com>
2023-03-17 22:38:22 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraformdockerbucket"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
