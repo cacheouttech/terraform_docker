6186728c99e45030988f64945cffa4acd344dc4c
cacheouttech <108108554+cacheouttech@users.noreply.github.com>
2023-03-17 22:39:04 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "az_region" {
+  description = "region to use for AWS resources"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "cidr" {
+  description = "CIDR range for created VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "private_cidr" {
+  description = "CIDR range for created VPC"
+  type        = string
+  default     = "10.10.1.0/24"
+}
